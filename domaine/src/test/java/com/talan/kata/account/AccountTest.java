33585fab3edd410c8b4844f3cc340d1cdd888355
moudanihamza <moudanihamza@gmail.com>
2019-04-13 20:39:24 +0100

    [TEST] defining the use case of deposit method

---
+++ domaine/src/test/java/com/talan/kata/account/AccountTest.java
@@ -0,0 +1,21 @@
+package com.talan.kata.account;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+
+    @Test
+    public void depositAmount(){
+        // a user should be able to deposit a valid amount
+        GIVEN:
+        // we had the right account with the specific amount
+        WHEN:
+        // the user deposit the x amount
+        THEN:
+        // if the amount is valid (positive) the older amount become as bellow else there will be a exception
+    }
+
+}
