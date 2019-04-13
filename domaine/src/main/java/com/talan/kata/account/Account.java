cceb168aee46728f99e65779f5e550228fdcb2e5
moudanihamza <moudanihamza@gmail.com>
2019-04-13 20:43:39 +0100

    [TEST] creation of deposit test

--- domaine/src/main/java/com/talan/kata/account/Account.java
+++ domaine/src/main/java/com/talan/kata/account/Account.java
@@ -55,5 +55,6 @@ public class Account {
     }
 
 
-
+    public void deposit(BigDecimal amountToDeposit) {
+    }
 }
--- domaine/src/test/java/com/talan/kata/account/AccountTest.java
+++ domaine/src/test/java/com/talan/kata/account/AccountTest.java
@@ -1,21 +1,33 @@
 package com.talan.kata.account;
 
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDateTime;
 
 import static org.junit.Assert.*;
 
 public class AccountTest {
 
     @Test
-    public void depositAmount(){
+    public void depositAmount() {
+        Account sut;
+        BigDecimal amountToDeposit = new BigDecimal("10.00");
+        BigDecimal actualAmount = new BigDecimal("20.00");
         // a user should be able to deposit a valid amount
         GIVEN:
         // we had the right account with the specific amount
+        sut = new Account(new AccountID("1"), "BB12", "toto", LocalDateTime.now(Clock.systemUTC()), actualAmount);
+
         WHEN:
         // the user deposit the x amount
+        sut.deposit(amountToDeposit);
         THEN:
-        // if the amount is valid (positive) the older amount become as bellow else there will be a exception
+        // if the amount is valid (positive) the older amount become as bellow
+        Assertions.assertThat(sut.getAmount()).isEqualByComparingTo(actualAmount.add(amountToDeposit));
     }
 
 }
